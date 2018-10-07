47d108343172a7a6fa5f9ef6f799a8e6678e5fbf
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-10-07 22:33:13 +0200

    add details page

---
+++ src/pages/details/details.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DetailsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-details',
+  templateUrl: 'details.html',
+})
+export class DetailsPage {
+
+  item : Object;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.get('data');
+    console.log(this.item);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailsPage');
+  }
+
+}
