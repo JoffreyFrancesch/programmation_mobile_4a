13d0d70be4c43440b61623f404906fa257ba223c
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-10-07 22:33:47 +0200

    add navPush to DetailsPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,25 +8,30 @@ import {
   AngularFireDatabase
 } from "@angular/fire/database";
 
+import { DetailsPage } from "../../pages/details/details" 
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
+
 export class HomePage {
-   items : Object;
+   items : Array<Object>;
 
   constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
     afDB
       .list("/film-list")
       .valueChanges()
-      .subscribe(data => {
+      .subscribe((data) => {
         this.items = data;
         console.log("items", this.items);
       });
   }
 
-  itemSelected(item: string) {
+  itemSelected(item: Object) {
+    this.navCtrl.push(DetailsPage, {
+      data : item
+    })
     console.log("Selected Item", item);
   }
 }
